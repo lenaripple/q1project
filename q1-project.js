ce05ea3ef67f1b5bdea90f3f969eee2d9fdfcb8a
Lena Ripple <ripplelena@gmail.com>
2017-03-15 10:32:39 -0600

    updated weather api

--- q1-project.js
+++ q1-project.js
@@ -10,30 +10,45 @@ var summary;
 var temp;
 var hike;
 var ski;
+var timeZone;
+var year;
+var month;
+var day;
+var time = 'T[14]:[00]:[00]'
 
 function findDate(event){
   event.preventDefault()
   date=$('#date').val()
-  console.log(date);
+  year = date.slice(0,4)
+  month = date.slice(5,7)
+  day = date.slice(8,10)
+  console.log(date)
 }
 
 function findLocation(event){
   event.preventDefault()
   locations=$('#location').val()
   if (locations==='Denver'){
-    locations = '39.7392,-104.9903'
+    locations = '39.7392,-104.9903';
+    timeZone='America/Denver'
   }
   else if (locations==="Boulder"){
-    locations = '40.0150,-105.2705'
+    locations = '40.0150,-105.2705';
+    timeZone='America/Denver'
   }
   else if (locations==="Telluride"){
     locations = '37.9375,-107.8123';
+    timeZone='America/Denver'
+
   }
   else if (locations==="Vail"){
-    locations ='39.6403,-106.3742'
+    locations ='39.6403,-106.3742';
+    timeZone='America/Denver'
   }
   else if (locations==="Portland"){
-    locations ='43.6615,-70.2553'
+    locations ='43.6615,-70.2553';
+    timeZone='America/New_York'
+
   }
   parseInt(locations)
   console.log(locations);
@@ -56,11 +71,13 @@ function getActivity(temp){
 
 function  getForecast(){
   var url = 'https://galvanize-cors.herokuapp.com/https://api.darksky.net/forecast/5816e128ecf24590e8e5af9d7b34dc03/'
-  url +=(locations+'?currently')
+  url +=(locations+'?'+'['+year+']'+'-'+'['+month+']'+'-'+'['+day+']'+time+'?currently')
+  console.log(url);
   $.get(url)
   .then(function(data) {
+    console.log(data);
     summary = data.daily.summary;
-    temp = data.currently.apparentTemperature
+    temp = data.daily.data[0].apparentTemperatureMax
     parseInt(temp);
     console.log(summary);
     console.log(temp);
@@ -95,61 +112,68 @@ function getBeer(){
     brewery=data[Math.floor(Math.random()*data.length)]
     brewery=(data[i].name+', '+data[i].street+', '+data[i].city+', '+data[i].state)
   }
-    $('.activity').append("Current temperature is "+temp+" degrees. <br> Forecast is: "+summary+"<br><br>Looks like beer weather! <br> Try this brewery: "+brewery)
+    $('.activity').append("The high will be "+temp+" degrees. <br> Forecast is: "+summary+"<br><br>The weather looks pretty lame. Better start drinking! <br> Try this brewery: "+brewery)
     })
   }
 
-  function getHike(){
-    if (locations==='39.7392,-104.9903'|| locations==='denver'){
-      hike = [{name:'Staunton Ranch and Bugling Elk Loop',
-              trailhead:'Staunton State Park, 12102 South Elk Creek Road, Pine, CO 80470',
-              miles:'8.6 miles long',
-              rating: 'rated as a moderate hike'}
-            ]
-    }
-    else if (locations==='40.0150,-105.2705'|| locations==='boulder'){
-      hike = [{name:'Shadow Canyon Trail to South Boulder Peak',
-              trailhead:'South Mesa Trailhead, 3633-, 4111 Eldorado Springs Dr, Boulder, CO 80303',
-              miles:'8.1 miles long',
-              rating: 'rated as a difficult hike'}
-            ]
-    }
-    else if (locations==='37.9375,-107.8123'|| locations==='telluride'){
-      hike = [{name:'Blue Lakes Trail',
-              trailhead:'Blue Lakes Trailhead, Dallas Creek Road off Highway 62',
-              miles:'6.3 miles long',
-              rating: 'rated as a moderate hike'}
-            ]
-    }
-    else if (locations==='39.6403,-106.3742'|| locations==='vail'){
-      hike = [{name:'Berrypicker Trail',
-              trailhead:'8 Lionshead Pl, Vail, CO 81657',
-              miles:'6.1 miles long',
-              rating: 'rated as a difficult hike'}
-            ]
-    }
-    else if (locations==="43.6615,-70.2553"||locations==='portland'){
-      hike = [{name:'Mount Katahdin and Hamlin Peak Loop',
-              trailhead:'Chimney Pond Trail, Millinocket, ME 04462',
-              miles:'10.0 miles long',
-              rating: 'rated as a difficult hike'}
-            ]
-    }
-    hike = (hike[0].name+', '+hike[0].trailhead+', '+hike[0].miles+', '+hike[0].rating)
-      $('.activity').append("The current temperature is "+temp+" degrees. <br> The weekend forecast is: "+summary+"<br><br>Looks like hiking weather! <br> Check this trail out: "+hike)
-    }
+function getHike(){
+  if (locations==='39.7392,-104.9903'|| locations==='denver'){
+    hike = [{name:'Staunton Ranch and Bugling Elk Loop',
+            trailhead:'Staunton State Park, 12102 South Elk Creek Road, Pine, CO 80470',
+            miles:'8.6 miles long',
+            rating: 'rated as a moderate hike'}
+          ]
+  }
+  else if (locations==='40.0150,-105.2705'|| locations==='boulder'){
+    hike = [{name:'Shadow Canyon Trail to South Boulder Peak',
+            trailhead:'South Mesa Trailhead, 3633-, 4111 Eldorado Springs Dr, Boulder, CO 80303',
+            miles:'8.1 miles long',
+            rating: 'rated as a difficult hike'}
+          ]
+  }
+  else if (locations==='37.9375,-107.8123'|| locations==='telluride'){
+    hike = [{name:'Blue Lakes Trail',
+            trailhead:'Blue Lakes Trailhead, Dallas Creek Road off Highway 62',
+            miles:'6.3 miles long',
+            rating: 'rated as a moderate hike'}
+          ]
+  }
+  else if (locations==='39.6403,-106.3742'|| locations==='vail'){
+    hike = [{name:'Berrypicker Trail',
+            trailhead:'8 Lionshead Pl, Vail, CO 81657',
+            miles:'6.1 miles long',
+            rating: 'rated as a difficult hike'}
+          ]
+  }
+  else if (locations==="43.6615,-70.2553"||locations==='portland'){
+    hike = [{name:'Mount Katahdin and Hamlin Peak Loop',
+            trailhead:'Chimney Pond Trail, Millinocket, ME 04462',
+            miles:'10.0 miles long',
+            rating: 'rated as a difficult hike'}
+          ]
+                
+  }
+  hike = (hike[0].name+', '+hike[0].trailhead+', '+hike[0].miles+', '+hike[0].rating)
+    $('.activity').append("The high will be "+temp+" degrees. <br> The weekend forecast is: "+summary+"<br><br>Looks like hiking weather! <br> Check this trail out: "+hike)
+  }
 
 function getSkiing(){
   var url = 'http://api.worldweatheronline.com/premium/v1/search.ashx?key=6580809fbcb644169e0212704171403&q='
-  console.log(locations);
   url+=(locations+'&wct=Ski&format=json')
   $.get(url)
   .then(function(data){
     console.log(data);
-    for (var i = 0; i < data.length; i++) {
-    ski=data[Math.floor(Math.random()*data.length)]
-    ski=(data[i].search_api.result[i].areaName[i].value+', '+data[i].search_api.result[i].region[i].value)
-    }
-    $('.activity').append("Current temperature is "+temp+" degrees. <br> Forecast is: "+summary+"<br><br>Looks like awesome skiing weather! <br> Try this resort: "+ski)
-    })
+    for (var i = 0; i < 10; i++) {
+      var resorts = []
+      resorts.push(data[i].search_api.result[i].areaName[i].value)
+      return resorts
+      // console.log(resorts);
+    // ski=data[Math.floor(Math.random()*data.length)]
+    // console.log(ski);
+    // ski=(data[i].search_api.result[i].areaName[i].value+', '+data[i].search_api.result[i].region[i].value)
   }
+  console.log(resorts);
+    $('.activity').append("The high will be "+temp+" degrees. <br> Forecast is: "+summary+"<br><br>Looks like awesome skiing weather! <br> Try this resort: "+ski)
+  })
+  }
+// .search_api.result["0"].areaName["0"].value
